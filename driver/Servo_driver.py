c3599b1b22ee104542be3fecb27c0c8e646eb6c4
mekhtiche <makhtichemed@yahoo.fr>
2017-01-11 15:32:23 +0000

    Update Servo_driver.py

--- driver/Servo_driver.py
+++ driver/Servo_driver.py
@@ -2,43 +2,18 @@
 import sys,os.path
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 import rospy
-from right_hand.msg import servoSet, servoCmd
+from std/msgs.msg import Int16
 from servo.Adafruit_PWM_Servo_Driver import PWM
 print "RIGHT SERVO_DRIVER successful import"
 Rpwm = PWM(0x40, 1)
 Rpwm.setPWMFreq(30)
 
-
-
-
-def doIt(Rcmd):
-    Rpwm.setPWM(1, 0, Rcmd[0])
-    Rpwm.setPWM(2, 0, Rcmd[1])
-    Rpwm.setPWM(3, 0, Rcmd[2])
-    Rpwm.setPWM(4, 0, Rcmd[3])
-    Rpwm.setPWM(5, 0, Rcmd[4])
-    Rpwm.setPWM(6, 0, Rcmd[5])
-    Rpwm.setPWM(7, 0, Rcmd[6])
-    Rpwm.setPWM(8, 0, Rcmd[7])
-    Rpwm.setPWM(9, 0, Rcmd[8])
-
-    
-
-
-def CALLBACK(data):
-    print data
-    doIt(data.cmd)
-
-def callback(data):
-    print data
-    Rpwm.setPWM(data.motor, 0, data.command)
-
-
+def callback(data, servo):
+    Rpwm.setPWM(servo, 0, data.data)
 
 if __name__ == '__main__':
-
     rospy.init_node('Right_hand_driver', anonymous=True)
-    rospy.Subscriber('servo/Rcmd', servoSet, callback=CALLBACK)
-    rospy.Subscriber('servo/rcmd', servoCmd, callback=callback)
+    for servo in range(1, 10):
+        rospy.Subscriber('servo/R' + str(servo), Int16, callback=callback, callback_args=servo)
     print 'RIGHT SERVO_DRIVER publishers & subscribers successful Initial'
     rospy.spin()
